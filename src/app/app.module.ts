2971db096f29f91f0b1e415ae0f74505b0315cd3
shahjay00785 <shahjay00785@gmail.com>
2018-07-22 13:44:26 +0530

    seperation of service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,5 @@
+import { PostService } from './services/post.service';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
@@ -7,7 +9,7 @@ import { HttpModule } from '@angular/http';
 @NgModule({
   declarations: [AppComponent, PostComponent],
   imports: [BrowserModule, HttpModule],
-  providers: [],
+  providers: [PostService],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
--- src/app/post/post.component.ts
+++ src/app/post/post.component.ts
@@ -1,17 +1,64 @@
+import { Http } from '@angular/http';
+import { PostService } from './../services/post.service';
 import { Component, OnInit } from '@angular/core';
-import { Http, Response } from '@angular/http';
 
 @Component({
   selector: 'app-post',
   templateUrl: './post.component.html',
   styleUrls: ['./post.component.css']
 })
-export class PostComponent {
+export class PostComponent implements OnInit {
   posts: any[];
+  constructor(private service: PostService) {}
 
+  ngOnInit() {
+    this.service.getPosts().subscribe(response => {
+      //console.log(response);
+      this.posts = response.json();
+      //console.log(response.json());
+    });
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post: any = { title: input.value };
+    input.value = '';
+    this.service.createPost(post).subscribe(response => {
+      post['id'] = response.json().id;
+      //console.log(response.json());
+      this.posts.splice(0, 0, post);
+    });
+  }
+
+  updatePost(post) {
+    this.service.updatePost(post).subscribe(response => {
+      console.log(response);
+    });
+  }
+
+  deletePost(post) {
+    this.service.deletePost(post.id).subscribe(response => {
+      let index = this.posts.indexOf('post');
+      //
+      this.posts.splice(index, 1);
+    });
+  }
+
+  /* Before Service Sepeation */
+  /*
+  posts: any[];
   private url = 'http://jsonplaceholder.typicode.com/posts';
+
   constructor(private http: Http) {
-    http.get(this.url).subscribe(response => {
+     http.get(this.url).subscribe(response => {
+      //console.log(response);
+      this.posts = response.json();
+      //console.log(response.json());
+    });
+    
+}
+
+  ngOnInit() {
+    this.http.get(this.url).subscribe(response => {
       //console.log(response);
       this.posts = response.json();
       //console.log(response.json());
@@ -43,4 +90,5 @@ export class PostComponent {
       //this.posts.splice(index, 1);
     });
   }
+  */
 }
